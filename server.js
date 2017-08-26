f59ccdfaab2cfb030ccd7f80032f266cb65a679a
Ngoc Tina Hoang <ngoc.tina.hoang@outlook.com>
2017-08-25 22:33:27 -0400

    updated server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const postRouter = require ()
 
 app.use(morgan('common'));
 
+app.use('/blog-posts', postRouter);
+
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
-
-app.use('/blog-posts', postRouter);
